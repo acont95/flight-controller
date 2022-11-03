4bb6a78d65c4ae3d4b129e59141b0669c70daaa8
Alex Conticello <alexconticello@gmail.com>
2022-11-03 16:46:10 -0400

    Implement pico spi bus

--- controller/include/pico_spi_bus.hpp
+++ controller/include/pico_spi_bus.hpp
@@ -1,15 +1,14 @@
-// #define DT_DRV_COMPAT raspberrypi_pico_spi
-
+#include <inttypes.h>
 #include <spi_bus.h>
-#include <zephyr/drivers/spi.h>
+#include <hardware/spi.h>
 
-class ZephyrSPIBus: public SPIBusMaster {
+class PicoSPIBus: public SPIBusMaster {
     public:
-        ZephyrSPIBus(spi_dt_spec* spi_spec);
+        PicoSPIBus(spi_inst_t& spi_instance);
         void lock();
         void unlock();
         uint8_t write(uint8_t data);
         void write(uint8_t tx_buffer[], uint32_t tx_length, uint8_t rx_buffer[], uint32_t rx_length);
     private:
-        spi_dt_spec* spi_spec;
+        spi_inst_t& spi_instance;
 };
